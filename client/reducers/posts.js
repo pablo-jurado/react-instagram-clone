808afbb2a96b1a313ee25c2db4075d7bc755f464
Pablo Jurado <pjurado@overnitecbt.com>
2017-06-15 15:40:29 -0500

    update likes state

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,37 @@
 
 function posts (state = [], action) {
   console.log(state, action)
+
+  if (action.type === 'INCREMENT_LIKES') {
+    console.log('likes!!!')
+    console.log(state)
+    const i = action.index;
+    return [
+             ...state.slice(0, i),
+             {...state[i],  likes: state[i].likes + 1 },
+             ...state.slice(i + 1)
+           ]
+  }
   return state
 }
 
 export default posts
+
+
+
+// function posts (state = [], action) {
+//   console.log(state, action)
+//
+//   if (action.type === 'INCREMENT_LIKES') {
+//     console.log('likes!!!')
+//     const i = action.index;
+//       return [
+//         ...state.slice(0, i),
+//         {...state[i],  likes: state[i].likes + 1 },
+//         ...state.slice(i + 1)
+//       ]
+//   }
+//   return state
+// }
+//
+// export default posts
