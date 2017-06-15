62432e0350df54fd8fc169630e8b0a675aa657cc
Pablo Jurado <pjurado@overnitecbt.com>
2017-06-15 11:29:01 -0500

    create and export the reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,9 +1,9 @@
 // reducers takes as arguments a copy of current sate and a action
 // we are pre setting the state as an empty array
 
-function posts (state = [], action) {
+function comments (state = [], action) {
   console.log(state, action)
   return state
 }
 
-export default posts
+export default comments
