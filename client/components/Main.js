183a5663c81be045dd01dcf73ac60bb3d75828a9
Pablo Jurado <pjurado@overnitecbt.com>
2017-06-15 09:56:20 -0500

    first commit

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+function Main () {
+  return (
+    <div>
+      <h1>
+        <Link to='/' >Instaclone</Link>
+      </h1>
+    </div>
+  )
+}
+
+export default Main
